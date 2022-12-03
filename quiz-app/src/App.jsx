3c87b1148e25f9e9731d0f0e56e9dbbeed5e6903
hyklops <hyklops@hotmail.com>
2022-12-03 16:56:07 +0300

    update

--- quiz-app/src/App.jsx
+++ quiz-app/src/App.jsx
@@ -1,83 +1,89 @@
 import React, { useState, useEffect } from "react";
 import Trivia from "./assets/components/Trivia";
 import "./index.css";
+import { nanoid } from "nanoid";
 
 function App() {
-    const [trivias, setTrivias] = useState(null);
-    const [isFetched, setIsFetched] = useState(false);
-    const [correctAnswers, setCorrectAnswers] = useState([]);
+  const [trivias, setTrivias] = useState(null);
+  const [isFetched, setIsFetched] = useState(false);
+  const [correctAnswers, setCorrectAnswers] = useState([]);
 
-   
-    async function dataFetch() {
-        const res = await fetch(
-            "https://opentdb.com/api.php?amount=5&type=multiple"
-        );
-        const data = await res.json();
-        return setTrivias(data.results);
-    }
-
-    const shuffle = (trivia) => {
-        const answers = [...trivia.incorrect_answers, trivia.correct_answer];
-        const shuffledAnswers = getRandomAnswers(answers);
-
-        // const shuffleArray = (answer) => [...array].sort(() => Math.random() - 0.5);
-        return shuffledAnswers;
-    };
-
-    const getRandomAnswers = (answers) => {
-        const newAnswers = [];
-
-        while (newAnswers.length !== 4) {
-            const getRandomIndex = Math.floor(Math.random() * 4);
-
-            const condition = newAnswers.some(
-                (item) => item === answers[getRandomIndex]
-            );
-
-            if (!condition) {
-                newAnswers.push(answers[getRandomIndex]);
-            }
-        }
-
-        return newAnswers;
-    };
-
-    const getTrivias = () => {
-        return trivias.map((trivia) => {
-            return <Trivia question={trivia.question} answers={shuffle(trivia)} />;
-        });
-    };
-
-    useEffect(() => {
-        dataFetch();
-    }, []);
-
-
-    useEffect(() => {
-        if (trivias) {
-            setIsFetched(true);
-            const updateCorrectAnswers = () => {
-                trivias.map((trivia) => { 
-                    if(correctAnswers.some(item => item===trivia.correct_answer)) {return} else setCorrectAnswers((prev) => [...prev, trivia.correct_answer]) ;
-                });
-            };
-            updateCorrectAnswers();
-            console.log({ correctAnswers });
-        }
-    }, [trivias,correctAnswers]);
-
-    if (!isFetched) {
-        return <h1>loading</h1>;
-    }
-
-    return (
-        <div className="App">
-            <div>{getTrivias()}</div>
-            <div className="check">
-                <button>Check Answers</button>
-            </div>
-        </div>
+  async function dataFetch() {
+    const res = await fetch(
+      "https://opentdb.com/api.php?amount=5&type=multiple"
     );
+    const data = await res.json();
+    return setTrivias(data.results);
+  }
+
+  const shuffle = (trivia) => {
+    const answers = [...trivia.incorrect_answers, trivia.correct_answer];
+    const shuffledAnswers = getRandomAnswers(answers);
+
+    // const shuffleArray = (answer) => [...array].sort(() => Math.random() - 0.5);
+    return shuffledAnswers;
+  };
+
+  const getRandomAnswers = (answers) => {
+    const newAnswers = [];
+
+    while (newAnswers.length !== 4) {
+      const getRandomIndex = Math.floor(Math.random() * 4);
+
+      const condition = newAnswers.some(
+        (item) => item === answers[getRandomIndex]
+      );
+
+      if (!condition) {
+        newAnswers.push(answers[getRandomIndex]);
+      }
+    }
+
+    return newAnswers;
+  };
+
+  const getTrivias = () => {
+    return trivias.map((trivia) => {
+      return (
+        <Trivia
+          question={trivia.question}
+          answers={shuffle(trivia)}
+          key={nanoid()}
+        />
+      );
+    });
+  };
+
+  useEffect(() => {
+    dataFetch();
+  }, []);
+
+  useEffect(() => {
+    if (trivias) {
+      setIsFetched(true);
+      const updateCorrectAnswers = () => {
+        trivias.map((trivia) => {
+          if (correctAnswers.some((item) => item === trivia.correct_answer));
+          else setCorrectAnswers((prev) => [...prev, trivia.correct_answer]);
+        });
+      };
+      updateCorrectAnswers();
+      console.log({ correctAnswers });
+    }
+  }, [trivias, correctAnswers]);
+
+  if (!isFetched) {
+    return <h1>loading</h1>;
+  }
+
+  return (
+    <div className="App">
+      <div>{getTrivias()}</div>
+      <div className="check">
+        <button>Check Answers</button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- quiz-app/src/assets/components/Trivia.jsx
+++ quiz-app/src/assets/components/Trivia.jsx
@@ -1,26 +1,29 @@
+import { nanoid } from "nanoid";
 import React from "react";
-import { useState } from "react";
 // buttonların clicklenip clinklenmediği state'e.
 
 // eğer button clicklenmişse ve resulta basılmışsa doğru butonları yeşil yap. eğer isClicked ile doğru seçenek farklı ise isClicked kırmızı olsun
 
 const Trivia = (props) => {
-    const renderAnswers = () => {
-        return props.answers.map((item, answerIndex) => (
-            <button className={`answer--${answerIndex + 1} answer ${"placeholder"}`}>
-                <div dangerouslySetInnerHTML={{ __html: item }} />
-            </button>
-        ));
-    };
+  const renderAnswers = () => {
+    return props.answers.map((item, answerIndex) => (
+      <button
+        key={nanoid()}
+        className={`answer--${answerIndex + 1} answer ${"placeholder"}`}
+      >
+        <div dangerouslySetInnerHTML={{ __html: item }} />
+      </button>
+    ));
+  };
 
-    return (
-        <div className="trivias">
-            <div className="question">
-                <div dangerouslySetInnerHTML={{ __html: props.question }} />
-            </div>
-            <div className="answers">{renderAnswers()}</div>
-        </div>
-    );
+  return (
+    <div className="trivias">
+      <div className="question">
+        <div dangerouslySetInnerHTML={{ __html: props.question }} />
+      </div>
+      <div className="answers">{renderAnswers()}</div>
+    </div>
+  );
 };
 
 export default Trivia;
