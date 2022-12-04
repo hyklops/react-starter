eeef39651a41c7f6d01d27c45abc591c9886e316
onur.sahal <onur.sahal@yolda.com>
2022-12-04 16:02:49 +0300

    feat: select answers function refactored

--- quiz-app/src/assets/components/Trivia.jsx
+++ quiz-app/src/assets/components/Trivia.jsx
@@ -1,33 +1,21 @@
-import { nanoid } from "nanoid";
+/* eslint-disable react/prop-types */
 import React from "react";
-// buttonların clicklenip clinklenmediği state'e.
-
-// eğer button clicklenmişse ve resulta basılmışsa doğru butonları yeşil yap. eğer isClicked ile doğru seçenek farklı ise isClicked kırmızı olsun
+import { nanoid } from "nanoid";
 
 const Trivia = (props) => {
-  const {
-    question,
-    selectedIndex,
-    answers,
-    answersArr,
-    selectAnswer,
-    isSelected,
-    questionIndex,
-  } = props;
-
+  const { question, answers, selectAnswer, questionIndex } = props;
   const renderAnswers = () => {
-    console.log({ answers });
-    return answers.map((item, answerIndex) => {
-      const styles = { backgroundColor: item.isSelected && "#59E391" };
+    return answers.map?.((item, answerIndex) => {
+      const styles = {
+        backgroundColor: item.isSelected ? "#59E391" : null,
+      };
 
       return (
         <button
           value={item.value}
           style={styles}
-          onClick={(e) =>
-            selectAnswer(e.target.value, questionIndex, answerIndex)
-          }
-          key={Date.now()}
+          onClick={() => selectAnswer(questionIndex, answerIndex)}
+          key={nanoid()}
           className={`answer--${answerIndex + 1} answer ${"placeholder"}`}
         >
           <div
